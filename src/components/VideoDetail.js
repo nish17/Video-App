ea254d53eaa2ecc6f44b7c95a1902e4b9906d6bc
Nimesh Solanki <nimesh3536@gmail.com>
2020-08-28 01:55:26 +0530

    try with onfocus

--- src/components/VideoDetail.js
+++ src/components/VideoDetail.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useRef } from 'react';
 import '../App.css';
 import VideoInfo from './VideoInfo';
 
@@ -6,10 +6,23 @@ export default function Videodetail({ videoData }) {
   const videoInfo = { ...videoData.video };
   const channelInfo = { ...videoData.channel };
   const userInfo = { ...channelInfo.user };
+
+  const videoRef = useRef(null);
+
   return (
     <section className='video-view'>
-      <video className='video-player'>
-        <source src={videoInfo.originalUrl} type='video/mp4' />
+      <video
+        className='video-player'
+        onFocus={() => videoRef.current.play()}
+        onBlur={() => videoRef.current.pause()}
+        ref={videoRef}
+        autoPlay
+      >
+        <source
+          src={videoInfo.originalUrl}
+          type='video/mp4'
+          allow='autoplay; fullscreen'
+        />
       </video>
       <div className='video-info-container'>
         <VideoInfo userName={userInfo.name} />
